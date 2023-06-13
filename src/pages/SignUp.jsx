0bff7ebb6f4ba473319583518eba01c18f808b74
Brent Doering <brentiscoding@gmail.com>
2023-06-13 14:30:14 -0600

    Loaded auth with firebase to sign up

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import {db} from '../firebase.config'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -23,6 +25,27 @@ function SignUp() {
 
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth() 
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+      const user = userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+      navigate('/')
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
 
     return (
       <>
@@ -33,7 +56,7 @@ function SignUp() {
           </p>
         </header>
         <main>
-          <form>
+          <form onSubmit={onSubmit}>
             <input type="text" className="nameInput" placeholder="Name" id="name" value={name} onChange={onChange} />
             <input type="email" className="emailInput" placeholder="Email" id="email" value={email} onChange={onChange} />
 
@@ -44,11 +67,11 @@ function SignUp() {
 
             <Link to='/forgot-password' className='forgotPasswordLink'>Forgot Password</Link>
 
-            <div className="signInBar">
-              <p className="signInText">
-                Sign In
+            <div className="signUpBar">
+              <p className="signUpText">
+                Sign Up
               </p>
-              <button className="signInButton">
+              <button className="signUpButton">
                 <ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
               </button>
             </div>
@@ -57,8 +80,8 @@ function SignUp() {
 
           {/* For Google OAuth Component Later */}
 
-          <Link to='/sign-up' className='registerLink'>
-            Sign Up
+          <Link to='/sign-in' className='registerLink'>
+            Sign In
           </Link>
         </main>
       </div>
